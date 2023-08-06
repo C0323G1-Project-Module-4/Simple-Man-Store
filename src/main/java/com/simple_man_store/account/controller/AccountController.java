5bd4e39d5526b2cb62d48535030e14b24a29d009
taiphat99 <taiphatlqp@gmail.com>
2023-08-06 19:26:38 +0700

    [SIMPLE_MAN][DANNY][ACCOUNT] update landing page and account functions

--- src/main/java/com/simple_man_store/account/controller/AccountController.java
+++ src/main/java/com/simple_man_store/account/controller/AccountController.java
@@ -6,30 +6,29 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
 @Controller
 public class AccountController {
     @RequestMapping(value = {""}, method = RequestMethod.GET)
-    public String welcomePage(Model model) {
-        model.addAttribute("title", "Welcome");
-        model.addAttribute("message", "This is welcome page!");
+    public String landingPage(Model model) {
         return "home";
     }
     @GetMapping("/account")
     public String accountDetail() {
-        return "account/account";
+        return "/account/detail";
     }
-    @GetMapping("/sign-up")
+    @GetMapping("/register")
     public String signUp() {
         return "sign-up";
     }
     @GetMapping("/login")
-    public String signIn() {
+    public String signIn(Model model) {
+//        if(error){
+//            model.addAttribute("error","Tài khoản không hợp lệ!");
+//        }
         return "sign-in";
     }
     @RequestMapping(value = "/admin", method = RequestMethod.GET)
@@ -38,11 +37,6 @@ public class AccountController {
         String userInfo = WebUtils.toString(loginedUser);
         model.addAttribute("userInfo", userInfo);
         return "adminPage";
-    }
-
-    @RequestMapping(value = "/sign-in", method = RequestMethod.GET)
-    public String loginPage(Model model) {
-        return "sign-in";
     }
 
     @RequestMapping(value = "/logoutSuccessful", method = RequestMethod.GET)
@@ -62,9 +56,9 @@ public class AccountController {
         User loginedUser = (User) ((Authentication) principal).getPrincipal();
 
         String userInfo = WebUtils.toString(loginedUser);
-        model.addAttribute("userInfo", userInfo);
+//        model.addAttribute("userInfo", userInfo);
 
-        return "userInfoPage";
+            return "userInfoPage";
     }
 
     @RequestMapping(value = "/403", method = RequestMethod.GET)
