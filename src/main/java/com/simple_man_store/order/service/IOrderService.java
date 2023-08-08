03cc186475569144e35a2ddcc0846e9eaf4604f1
nvt2802 <nvt280202@gmail.com>
2023-08-08 09:45:56 +0700

    order

---
+++ src/main/java/com/simple_man_store/order/service/IOrderService.java
@@ -0,0 +1,17 @@
+package com.simple_man_store.order.service;
+
+import com.simple_man_store.account.model.Account;
+import com.simple_man_store.order.model.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface IOrderService {
+    void add(Order order);
+    Page<Order> findAllInPage(Pageable pageable, String searchName);
+
+    Order getById(int id);
+
+    boolean delete(int id);
+
+    Page<Order> getByAcount(Pageable pageable, Account account);
+}
