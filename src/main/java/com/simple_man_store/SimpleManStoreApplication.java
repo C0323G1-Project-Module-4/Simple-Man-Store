c7cc99c16cbc120da0a2ec8b670f5c54f2a37c41
nvt2802 <nvt280202@gmail.com>
2023-08-11 14:08:43 +0700

    update order

--- src/main/java/com/simple_man_store/SimpleManStoreApplication.java
+++ src/main/java/com/simple_man_store/SimpleManStoreApplication.java
@@ -2,8 +2,10 @@ package com.simple_man_store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SimpleManStoreApplication {
 
     public static void main(String[] args) {
